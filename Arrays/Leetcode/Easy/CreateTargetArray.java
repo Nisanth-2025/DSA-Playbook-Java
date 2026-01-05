f5a12288e8333da9b0fe9c5dc3949f2ecf0b7041
Nisanth2025 <Nisanth252025@gmail.com>
2026-01-05 10:29:13 +0530

    DSA(Arrays/Leetcode/Easy): Add CreateTargetArray

---
+++ Arrays/Leetcode/Easy/CreateTargetArray.java
@@ -0,0 +1,10 @@
+/**
+ * Problem: Create Target Array in the Given Order
+ * LeetCode: 1389
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/create-target-array-in-the-given-order/
+ *
+ * Pattern: Array Traversal
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(n)
+ */
