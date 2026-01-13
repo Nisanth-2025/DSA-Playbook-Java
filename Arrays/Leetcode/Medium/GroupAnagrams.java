7bb789da6afc13e1edeb4ea7eba2720a713d00bc
Nisanth2025 <Nisanth252025@gmail.com>
2026-01-13 21:21:20 +0530

    DSA(Arrays/Leetcode/Medium):Add Geoup Anagrams

---
+++ Arrays/Leetcode/Medium/GroupAnagrams.java
@@ -0,0 +1,31 @@
+/**
+ * Problem: Group Anagrams
+ * LeetCode: 49
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/group-anagrams/
+ *
+ * Pattern: Array Traversal, Hashing
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs)
+    {
+        HashMap<String,List<String>> res = new HashMap<>();
+        for(String s : strs)
+        {
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            if(!res.containsKey(key))
+            {
+                res.put(key,new ArrayList<>());
+            }
+            res.get(key).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
