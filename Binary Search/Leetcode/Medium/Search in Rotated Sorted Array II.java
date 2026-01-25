fdff7293ec041e1ce9f01d6fa683168480c5fa14
Nisanth2025 <Nisanth252025@gmail.com>
2026-01-25 23:51:10 +0530

    DSA(Binary Search/Leetcode/Medium):Add Search in Rotated Sorted Array II

---
+++ Binary Search/Leetcode/Medium/Search in Rotated Sorted Array II.java
@@ -0,0 +1,55 @@
+/**
+ * Problem: Search in Rotated Sorted Array II
+ * LeetCode: 81
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+ *
+ * Pattern: Divide and Conquer
+ * Time Complexity: O(log n)
+ * Space Complexity: O(1)
+ */
+
+class Solution 
+{
+    public boolean search(int[] nums, int target) 
+    {
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right)
+        {
+            int mid = left+(right-left)/2;
+            if(nums[mid]==target)
+            {
+                return true;
+            }
+            if(nums[left]==nums[mid] && nums[mid]==nums[right])
+            {
+                left++;
+                right--;
+            }
+            else if(nums[left]<=nums[mid])
+            {
+                if(target>=nums[left] && target<nums[mid])
+                {
+                    right = mid-1;
+                }
+                else
+                {
+                    left = mid+1;
+                }
+            }
+            else
+            {
+                if(target>nums[mid] && target<=nums[right])
+                {
+                    left = mid+1;
+                }
+                else
+                {
+                    right = mid-1;
+                }
+            }
+        }
+        return false;    
+    }
+}
